6852177f6101c3fbd26b1b0aee662aaa6217107f
curiosityr0ver <mehta.ishu14@gmail.com>
2023-05-28 15:28:15 +0530

    ItAllGoesToShit2

--- src/bankComponents/AboutUs.js
+++ src/bankComponents/AboutUs.js
@@ -1,16 +1,9 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
+import CustomerCard from './CustomerCard.js'
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
@@ -29,7 +22,7 @@ function Copyright() {
     );
 }
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2, 3, 4, 5, 6];
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
@@ -45,53 +38,18 @@ export default function Album() {
                         pb: 6,
                     }}
                 >
-                    <Container maxWidth="sm">
+                    <Container maxWidth="md">
                         <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                            Something short and leading about the collection below—its contents,
-                            the creator, etc. Make it short and sweet, but not too short so folks
-                            don&apos;t simply skip over it entirely.
+                            At Wells Fargo, our mission is to revolutionize the financial landscape by harnessing cutting-edge technology and create a seamless digital ecosystem that empowers individuals to take control of their financial future. We are driven by a passion for innovation, a dedication to excellence, and a vision of a world where financial services are not just accessible, but truly empowering for all.
                         </Typography>
-                        <Stack
-                            sx={{ pt: 4 }}
-                            direction="row"
-                            spacing={2}
-                            justifyContent="center"
-                        >
-                            <Button variant="contained">Main call to action</Button>
-                            <Button variant="outlined">Secondary action</Button>
-                        </Stack>
                     </Container>
                 </Box>
                 <Container sx={{ py: 8 }} maxWidth="md">
                     {/* End hero unit */}
                     <Grid container spacing={4}>
-                        {cards.map((card) => (
+                        {cards.map((card, index) => (
                             <Grid item key={card} xs={12} sm={6} md={4}>
-                                <Card
-                                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                                >
-                                    <CardMedia
-                                        component="div"
-                                        sx={{
-                                            // 16:9
-                                            pt: '56.25%',
-                                        }}
-                                        image="https://source.unsplash.com/random?wallpapers"
-                                    />
-                                    <CardContent sx={{ flexGrow: 1 }}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Heading
-                                        </Typography>
-                                        <Typography>
-                                            This is a media card. You can use this section to describe the
-                                            content.
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small">View</Button>
-                                        <Button size="small">Edit</Button>
-                                    </CardActions>
-                                </Card>
+                                <CustomerCard empid={index} />
                             </Grid>
                         ))}
                     </Grid>
--- src/bankComponents/CustomerCard.js
+++ src/bankComponents/CustomerCard.js
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import './CustomerCard.css'
 import { CardActionArea } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
-import IconButton from './PopUp/IconButton';
+
 
 
 export default function ActionAreaCard({ empid = 6, title = "Jose Leandro Torres", position = "CEO", description = "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica" }) {
@@ -22,11 +20,12 @@ export default function ActionAreaCard({ empid = 6, title = "Jose Leandro Torres
     ]
 
     const titles = [
-        "Jose Leandro Torres",
+        "Jose Torres",
         "Reinier Guerra",
         "Saray Pacheco",
         "Tahini Leon",
         "Anjali Verma",
+        "Kim Kardashiyan"
     ]
 
     const positions = [
@@ -47,53 +46,47 @@ export default function ActionAreaCard({ empid = 6, title = "Jose Leandro Torres
         "Graphic Designer with a keen eye for detail and a passion for creating beautiful and user-friendly designs.",
         "Sales Representative with a proven track record of closing deals and generating revenue.",
         "Accountant with a deep understanding of the customer journey and a commitment to providing excellent customer service."
-
     ]
 
     return (
         <div className='customerCard'>
             <img className='avatar' src={imgsrc[empid]} alt="" />
-            <Card className='card' sx={{ maxWidth: 345 }}>
-                <CardActionArea>
 
-                    <CardContent>
-                        <center>
-                            <div className='height2'></div>
-                            <h1 className='font1'>
-                                {titles[empid]}
-                            </h1>
-                            <h3 className='font2'>
-                                {positions[empid]}
-                            </h3>
-                            <p className='font3'>
-                                {descriptions[empid]}
-                            </p>
-                        </center>
-                        <div className='height2'></div>
-                        <table>
-                            <tr>
-                                <td>
-                                    <img className='smallicon' src={require("./179-1791856_official-facebook-circle-icon-hd-png-download.png")} alt="" />
-                                </td>
-                                <td>
-                                    <img className='smallicon' src={require("./d12e6800-b47a-11e9-85a3-5fe53e198d49.png")} alt="" />
-                                </td>
-                                <td>
-                                    <img className='smallicon' src={require("./youtube-logo-icon-vector-27990507.jpg")} alt="" />
-                                </td>
-                                <td>
-                                    <img className='smallicon' src={require("./702300.png")} alt="" />
-                                </td>
-                                <td>
-                                    <img className='smallicon' src={require("./telegram-logo-icon-vector-27990432.jpg")} alt="" />
-                                </td>
-                            </tr>
-                        </table>
+            <div className='card'>
+                <div className='height'></div>
 
+                <center>
+                    <h1 className='font1'>
+                        {titles[empid]}
+                    </h1>
+                    <h3 className='font2'>
+                        {positions[empid]}
+                    </h3>
+                    <p className='font3'>
+                        {descriptions[empid]}
+                    </p>
+                </center>
 
-                    </CardContent>
-                </CardActionArea >
-            </Card >
+                <table>
+                    <tr>
+                        <td>
+                            <img className='smallicon' src={require("./179-1791856_official-facebook-circle-icon-hd-png-download.png")} alt="" />
+                        </td>
+                        <td>
+                            <img className='smallicon' src={require("./d12e6800-b47a-11e9-85a3-5fe53e198d49.png")} alt="" />
+                        </td>
+                        <td>
+                            <img className='smallicon' src={require("./youtube-logo-icon-vector-27990507.jpg")} alt="" />
+                        </td>
+                        <td>
+                            <img className='smallicon' src={require("./702300.png")} alt="" />
+                        </td>
+                        <td>
+                            <img className='smallicon' src={require("./telegram-logo-icon-vector-27990432.jpg")} alt="" />
+                        </td>
+                    </tr>
+                </table>
+            </div >
         </div>
 
     );
